3fe2520a7681f7f6ae0d758b8c3fec23dffe8160
odonML <odon_ml@outlook.com>
2026-03-10 18:39:15 -0600

    add log total lines

--- dist/index.js
+++ dist/index.js
@@ -32892,7 +32892,7 @@ try {
     
     const pullRequest = context$1.payload.pull_request;
     const totalChanges = pullRequest.additions + pullRequest.deletions;
-
+    console.log(`Total changes (additions + deletions): ${totalChanges}`);
     let complexity = "no changes";
     if(totalChanges > 1 && totalChanges <= minimalThreshold) {
         complexity = "minimal";
